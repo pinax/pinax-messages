314a74a019ff81c56fe74bfda21fd44db6d0c68d
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-04 12:46:31 -0400

    pep8

--- user_messages/managers.py
+++ user_messages/managers.py
@@ -1,8 +1,10 @@
 from django.db.models import Manager, Q
 
+
 class ThreadManager(Manager):
     def inbox(self, user):
         return self.filter(Q(user_1=user, user1_deleted=False) | Q(user_2=user, user2_deleted=False))
+
 
 class MessageManager(Manager):
     def new_reply(self, thread, user, content):
