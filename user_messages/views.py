d855e5626ee639a237467af7f6f57947cd17f9c4
Alex Gaynor <alex.gaynor@gmail.com>
2009-09-04 13:55:36 -0400

    Add reply support to threads

--- user_messages/views.py
+++ user_messages/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 
+from user_messages.forms import MessageReplyForm
 from user_messages.models import Thread, Message
 
 @login_required
@@ -16,9 +19,19 @@ def thread_detail(request, thread_id,
     template_name='user_messages/thread_detail.html'):
     qs = Thread.objects.filter(Q(to_user=request.user) | Q(from_user=request.user))
     thread = get_object_or_404(qs, pk=thread_id)
-    if request.user == thread.to_user:
-        thread.to_user_unread = False
+    if request.method == 'POST':
+        form = MessageReplyForm(request.POST, user=requst.user, thread=thread)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse(inbox))
     else:
-        thread.from_user_unread = False
-    thread.save()
-    return render_to_response(template_name, {'thread': thread}, context_instance=RequestContext(request))
+        form = MessageReplyForm(user=request.user, thread=thread)
+        if request.user == thread.to_user:
+            thread.to_user_unread = False
+        else:
+            thread.from_user_unread = False
+        thread.save()
+    return render_to_response(template_name, {
+        'thread': thread,
+        'form': form
+    }, context_instance=RequestContext(request))
