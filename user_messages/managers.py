7ea131b0c906c8da66f050e5833ded02f8acb495
Alex Gaynor <alex.gaynor@gmail.com>
2009-10-28 13:54:47 -0400

    Send a signal on each message that is sent to allow for external customization.

--- user_messages/managers.py
+++ user_messages/managers.py
@@ -1,5 +1,7 @@
 from django.db.models import Manager
 
+from user_messages.signals import message_sent
+
 
 class ThreadManager(Manager):
     def inbox(self, user):
@@ -13,6 +15,7 @@ class MessageManager(Manager):
     def new_reply(self, thread, user, content):
         msg = self.create(thread=thread, sender=user, content=content)
         thread.userthread_set.exclude(user=user).update(deleted=False, unread=True)
+        message_sent.send(sender=self.model, message=msg, thread=thread)
         return msg
     
     def new_message(self, from_user, to_users, subject, content):
@@ -21,4 +24,6 @@ class MessageManager(Manager):
         for user in to_users:
             thread.userthread_set.create(user=user, deleted=False, unread=True)
         thread.userthread_set.create(user=from_user, deleted=True, unread=False)
-        return self.create(thread=thread, sender=from_user, content=content)
+        msg = self.create(thread=thread, sender=from_user, content=content)
+        message_sent.send(sender=self.model, message=msg, thread=thread)
+        return msg
