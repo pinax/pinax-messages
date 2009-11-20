52ee4e37703bda72ec88edbcdd0f034468d6a439
Brian Rosner <brosner@gmail.com>
2009-11-20 09:09:22 -0700

    added more metadata in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ from setuptools import setup, find_packages
 setup(
     name = "user_messages",
     version = "0.1",
+    author = "Eldarion",
+    author_email = "development@eldarion.com",
+    description = "Private user messages for Django",
+    license = "Commerical",
     packages = find_packages(),
     zip_safe = False,
 )
