285fbed3430a2634bd489d8d7b5e99f9e72f8d46
Brian Rosner <brosner@gmail.com>
2010-04-28 15:58:22 -0600

    updated setup.py to use distutils not setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 
 setup(
@@ -6,8 +6,21 @@ setup(
     version = "0.1.dev2",
     author = "Eldarion",
     author_email = "development@eldarion.com",
-    description = "Private user messages for Django",
+    description = "a reusable private user messages application for Django",
+    long_description = open("README.rst").read(),
     license = "Commerical",
-    packages = find_packages(),
-    zip_safe = False,
+    url = "http://github.com/eldarion/user_messages",
+    packages = [
+        "user_messages",
+        "user_messages.tests",
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
 )
