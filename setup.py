d7bd46282df90fab2313c0dbe9551f10bef006bf
Brian Rosner <brosner@gmail.com>
2010-08-24 14:41:41 -0600

    0.1.dev4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "user-messages",
-    version = "0.1.dev3",
+    version = "0.1.dev4",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "a reusable private user messages application for Django",
