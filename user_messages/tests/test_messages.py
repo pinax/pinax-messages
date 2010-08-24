fb2a3d95c8798dcbd22c8fafc2f341e3b4875a97
Alex Gaynor <alex.gaynor@gmail.com>
2010-08-24 10:50:10 -0500

    COnvert to use eldarion.test

--- user_messages/tests/test_messages.py
+++ user_messages/tests/test_messages.py
@@ -2,9 +2,10 @@ import os
 
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.test import TestCase
 
 from django.contrib.auth.models import User
+
+from eldarion.test import TestCase
 
 from user_messages.models import Thread, Message
 
@@ -15,7 +16,6 @@ class BaseTest(TestCase):
             "brosner@brosner.brosner", "abc123")
         self.jtauber = User.objects.create_superuser("jtauber",
             "jtauber@jtauber.jtauber", "abc123")
-        self.client.login(username="brosner", password="abc123")
         if hasattr(self, "template_dirs"):
             self._old_template_dirs = settings.TEMPLATE_DIRS
             settings.TEMPLATE_DIRS = self.template_dirs
@@ -62,72 +62,71 @@ class TestMessageViews(BaseTest):
     template_dirs = [
         os.path.join(os.path.dirname(__file__), "templates")
     ]
-    
-    def tearDown(self):
-        self.client.logout()
-            
+
     def test_create_message(self):
-        response = self.client.get(reverse("messages_inbox"))
-        self.assertEqual(response.status_code, 200)
-        
-        response = self.client.get(reverse("message_create"))
-        self.assertEqual(response.status_code, 200)
-        
-        data = {
-            "subject": "The internet is down.",
-            "content": "Does this affect any of our sites?",
-            "to_user": str(self.jtauber.id)
-        }
-        
-        response = self.client.post(reverse("message_create"), data)
-        self.assertEqual(response.status_code, 302)
-        
-        self.assertEqual(Thread.objects.inbox(self.jtauber).count(), 1)
-        self.assertEqual(Thread.objects.inbox(self.brosner).count(), 0)
-        
-        response = self.client.get(reverse("message_create", kwargs={
-            "user_id": self.jtauber.id
-        }))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "selected=\"selected\">jtauber</option>")
-        
-        thread_id = Thread.objects.inbox(self.jtauber).get().id
-        
-        response = self.client.get(reverse("messages_thread_detail", kwargs={
-            "thread_id": thread_id,
-        }))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Does this affect any of our sites?")
-        
-        self.client.logout()
-        self.client.login(username="jtauber", password="abc123")
-        
-        response = self.client.get(reverse("messages_inbox"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Does this affect")
-        
-        response = self.client.get(reverse("messages_thread_detail", kwargs={
-            "thread_id": thread_id,
-        }))
-        self.assertContains(response, "Does this affect")
-        
-        response = self.client.post(reverse("messages_thread_delete", kwargs={
-            "thread_id": thread_id,
-        }))
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Thread.objects.inbox(self.jtauber).count(), 0)
-        
-        data = {
-            "content": "Nope, the internet being down doesn't affect us.",
-        }
-        
-        response = self.client.post(reverse("messages_thread_detail", kwargs={
-            "thread_id": thread_id,
-        }), data)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(Thread.objects.inbox(self.brosner).count(), 1)
-        self.assertEqual(Thread.objects.inbox(self.brosner).get().messages.count(), 2)
-        self.assertEqual(Thread.objects.unread(self.jtauber).count(), 0)
+        with self.login("brosner", "abc123"):
+            response = self.client.get(reverse("messages_inbox"))
+            self.assertEqual(response.status_code, 200)
+            
+            response = self.client.get(reverse("message_create"))
+            self.assertEqual(response.status_code, 200)
+            
+            data = {
+                "subject": "The internet is down.",
+                "content": "Does this affect any of our sites?",
+                "to_user": str(self.jtauber.id)
+            }
+            
+            response = self.client.post(reverse("message_create"), data)
+            self.assertEqual(response.status_code, 302)
+            
+            self.assertEqual(Thread.objects.inbox(self.jtauber).count(), 1)
+            self.assertEqual(Thread.objects.inbox(self.brosner).count(), 0)
+            
+            response = self.client.get(reverse("message_create", kwargs={
+                "user_id": self.jtauber.id
+            }))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "selected=\"selected\">jtauber</option>")
+            
+            thread_id = Thread.objects.inbox(self.jtauber).get().id
+            
+            response = self.client.get(reverse("messages_thread_detail", kwargs={
+                "thread_id": thread_id,
+            }))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "Does this affect any of our sites?")
+            
+        with self.login("jtauber", "abc123"):
+            response = self.client.get(reverse("messages_inbox"))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "Does this affect")
+            
+            response = self.client.get(reverse("messages_thread_detail", kwargs={
+                "thread_id": thread_id,
+            }))
+            self.assertContains(response, "Does this affect")
+            
+            response = self.client.post(reverse("messages_thread_delete", kwargs={
+                "thread_id": thread_id,
+            }))
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(Thread.objects.inbox(self.jtauber).count(), 0)
+            
+            data = {
+                "content": "Nope, the internet being down doesn't affect us.",
+            }
+            
+            response = self.client.post(reverse("messages_thread_detail", kwargs={
+                "thread_id": thread_id,
+            }), data)
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(Thread.objects.inbox(self.brosner).count(), 1)
+            self.assertEqual(
+                Thread.objects.inbox(self.brosner).get().messages.count(),
+                2
+            )
+            self.assertEqual(Thread.objects.unread(self.jtauber).count(), 0)
     
     def test_urls(self):
         self.assertEqual(reverse("message_create", args=[10]), "/create/10/")
