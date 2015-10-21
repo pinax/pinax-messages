4b1929b559283c4fe461a4d7876ac72c330c55b5
Patrick Altman <paltman@gmail.com>
2015-10-21 12:33:54 -0500

    Add admin.py

---
+++ pinax/messages/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import UserThread, Message
+
+
+class UserThreadAdmin(admin.ModelAdmin):
+    list_display = ["thread", "user", "unread", "deleted"]
+    list_filter = ["unread", "deleted"]
+    raw_id_fields = ["user"]
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ["thread", "sender", "sent_at"]
+    list_filter = ["sent_at", "thread"]
+    raw_id_fields = ["sender"]
+
+
+admin.site.register[Message, MessageAdmin]
+admin.site.register[UserThread, UserThreadAdmin]
