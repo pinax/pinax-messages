246531de80805b5288615104dbd38355135c4d95
arthur <arthur@wallstreetweb.net>
2016-02-20 19:30:46 +0100

    Use class based view for create and delete
    Closes #15

--- pinax/messages/urls.py
+++ pinax/messages/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = patterns(
     "",
     url(r"^inbox/$", views.InboxView.as_view(), name="messages_inbox"),
-    url(r"^create/$", views.message_create, name="message_create"),
-    url(r"^create/(?P<user_id>\d+)/$", views.message_create, name="message_create"),
+    url(r"^create/$", views.MessageCreateView.as_view(), name="message_create"),
+    url(r"^create/(?P<user_id>\d+)/$", views.MessageCreateView.as_view(), name="message_create"),
     url(r"^thread/(?P<pk>\d+)/$", views.ThreadView.as_view(), name="messages_thread_detail"),
-    url(r"^thread/(?P<thread_id>\d+)/delete/$", views.thread_delete, name="messages_thread_delete"),
+    url(r"^thread/(?P<pk>\d+)/delete/$", views.ThreadDeleteView.as_view(), name="messages_thread_delete"),
 )
