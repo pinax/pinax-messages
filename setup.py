279b078dca147fa518fc236814d7dab024a812e4
Graham Ullrich <graham@flyingcracker.com>
2016-07-26 17:27:37 -0600

    Remove templates, require PTB 7.9.0+

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     test_suite="runtests.runtests",
     tests_require=[
         "django-test-plus>=1.0.11",
-        "pinax-theme-bootstrap>=7.4.0",
+        "pinax-theme-bootstrap>=7.9.0",
     ],
     install_requires=[
         "django-appconf>=1.0.1",
