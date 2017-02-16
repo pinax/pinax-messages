747f67a46e82bbf7553fa4154ef80c76b30b9cc5
root <root@websitrescue.nz>
2017-02-16 17:40:49 +0000

    Add unread_threads templatetag for showing number of unread threads for the user

--- pinax/messages/templatetags/pinax_messages_tags.py
+++ pinax/messages/templatetags/pinax_messages_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+from pinax.messages.models import Thread
 
 register = template.Library()
 
@@ -7,3 +8,7 @@ register = template.Library()
 @register.filter
 def unread(thread, user):
     return bool(thread.userthread_set.filter(user=user, unread=True))
+
+@register.filter
+def unread_threads(user):
+    return len(Thread.unread(user))
