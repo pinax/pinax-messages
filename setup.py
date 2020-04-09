7a21deafca8c8a1b0b77966e8949b5abdf37e796
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-04-09 11:33:38 +0100

    Update third-party dependencies in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Pinax Messages
 .. image:: https://img.shields.io/pypi/v/pinax-messages.svg
     :target: https://pypi.python.org/pypi/pinax-messages/
 
-\ 
+\
 
 .. image:: https://img.shields.io/circleci/project/github/pinax/pinax-messages.svg
     :target: https://circleci.com/gh/pinax/pinax-messages
@@ -25,14 +25,14 @@ Pinax Messages
 .. image:: https://img.shields.io/github/issues-pr-closed/pinax/pinax-messages.svg
     :target: https://github.com/pinax/pinax-messages/pulls?q=is%3Apr+is%3Aclosed
 
-\ 
+\
 
 .. image:: http://slack.pinaxproject.com/badge.svg
     :target: http://slack.pinaxproject.com/
 .. image:: https://img.shields.io/badge/license-MIT-blue.svg
     :target: https://opensource.org/licenses/MIT/
 
-\ 
+\
 
 ``pinax-messages`` is a well tested, documented, and proven solution for any site that
 that wants to support private user-to-user messaging.
@@ -82,10 +82,10 @@ setup(
     ],
     install_requires=[
         "django>=1.11",
-        "django-appconf>=1.0.2",
+        "django-appconf>=1.0.4",
     ],
     tests_require=[
-        "django-test-plus>=1.0.22",
+        "django-test-plus>=1.4.0",
     ],
     test_suite="runtests.runtests",
     zip_safe=False
