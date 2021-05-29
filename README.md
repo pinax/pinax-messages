194ce4bd115c3223182bc8699e87b8c7fac803eb
Katherine Michel <kthrnmichel@gmail.com>
2021-05-28 23:35:52 -0500

    Update test matrix

--- README.md
+++ README.md
@@ -61,10 +61,11 @@ Where you can find what you need:
 
 #### Supported Django and Python Versions
 
-Django / Python | 3.6 | 3.7 | 3.8
---------------- | --- | --- | ---
-2.2  |  *  |  *  |  *
-3.0  |  *  |  *  |  *
+Django / Python | 3.6 | 3.7 | 3.8 | 3.9
+--------------- | --- | --- | --- | ---  
+2.2  |  *  |  *  |  *  |  *   
+3.1  |  *  |  *  |  *  |  *  
+3.2  |  *  |  *  |  *  |  *  
 
 
 ## Documentation
--- setup.py
+++ setup.py
@@ -40,13 +40,15 @@ that wants to support private user-to-user messaging.
 Supported Django and Python Versions
 ------------------------------------
 
-+-----------------+-----+-----+-----+
-| Django / Python | 3.6 | 3.7 | 3.8 |
-+=================+=====+=====+=====+
-|  2.2            |  *  |  *  |  *  |
-+-----------------+-----+-----+-----+
-|  3.0            |  *  |  *  |  *  |
-+-----------------+-----+-----+-----+
++-----------------+-----+-----+-----+-----+
+| Django / Python | 3.6 | 3.7 | 3.8 | 3.9 |
++=================+=====+=====+=====+=====+
+|  2.2            |  *  |  *  |  *  |  *  |
++-----------------+-----+-----+-----+=====+
+|  3.1            |  *  |  *  |  *  |  *  |
++-----------------+-----+-----+-----+=====+
+|  3.2            |  *  |  *  |  *  |  *  |
++-----------------+-----+-----+-----+-----+
 """
 
 setup(
@@ -67,7 +69,8 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Django",
         "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
@@ -76,6 +79,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
