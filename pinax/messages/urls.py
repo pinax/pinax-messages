b9f15efe194b301bdace6c836983a6b0c39b20f5
sg <sg@novel-t.ch>
2023-01-18 15:06:48 +0100

    Fix URL

--- pinax/messages/urls.py
+++ pinax/messages/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "pinax_messages"
 
 urlpatterns = [
-    path("inbox/", views.InboxView.as_view(), name="inbox"),
+    path("inbox/", views.inbox, name="inbox"),
     path("create/", views.MessageCreateView.as_view(), name="message_create"),
     path("thread/<int:pk>/", views.ThreadView.as_view(), name="thread_detail"),
     path("thread/<int:pk>/delete/", views.ThreadDeleteView.as_view(), name="thread_delete"),
